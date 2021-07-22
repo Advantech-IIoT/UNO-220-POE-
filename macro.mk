f2ae34a988f52247847c828419bef704d2901845
Yuchun <yuchun.chen@advantech.com.tw>
2021-07-22 11:46:38 +0800

    Update macro.mk for new deb packages.

--- macro.mk
+++ macro.mk
@@ -1,4 +1,4 @@
-##################################
+        ##################################
 # folders                        #
 ##################################
 currdir=$(shell pwd)
@@ -54,6 +54,7 @@ export SHELL=/bin/bash
 ##################################
 # dpkg                           #
 ##################################
+#uno220config
 dpkgconfigname=uno220config
 dpkgconfigversion=0.1
 dpkgconfigrevision=2
@@ -61,14 +62,17 @@ dpkgconfigarch=armhf
 dpkgconfigdesc=Advantech UNO-220 (Raspberry Pi 4) IO Card RTC Package for config.txt and cmdline.txt
 dpkgconfigeditor=Ralph Wang <ralph.wang@advantech.com.tw>
 dpkgconfigdepends=sed (>=4.7-1)
+#uno220rtc
 dpkgrtcname=uno220rtc
 dpkgrtcversion=0.1
-dpkgrtcrevision=2
+dpkgrtcrevision=5
 dpkgrtcarch=armhf
 dpkgrtcdesc=Advantech UNO-220 (Raspberry Pi 4) IO Card RTC Package for EPSON RTC RX8010
 dpkgrtceditor=Ralph Wang <ralph.wang@advantech.com.tw>
 dpkgrtcdepends=sed (>=4.7-1)
-dpkgrtcpredepends=raspberrypi-kernel (<=1.20201022-1)
+dpkgrtcpredepends=raspberrypi-kernel (<=1.20210430-1)
+#dpkgrtcpredepends=raspberrypi-kernel (<=1.20201022-1)
+#uno220gpio
 dpkggpioname=uno220gpio
 dpkggpioversion=0.1
 dpkggpiorevision=2
@@ -76,6 +80,7 @@ dpkggpioarch=armhf
 dpkggpiodesc=Advantech UNO-220 (Raspberry PI 4) IO Card GPIO EXPANDER for TI TCA9554 
 dpkggpioeditor=Ralph Wang <ralph.wang@advantech.com.tw>
 dpkggpiodepends=sed (>=4.7-1)
+#uno220uart
 dpkguartname=uno220uart
 dpkguartversion=0.1
 dpkguartrevision=2
