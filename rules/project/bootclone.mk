316a96b55a483afdf19b5cb5bdc77479713debed
Yuchun-Chen-ADV <yuchun.chen@advantech.com.tw>
2022-07-19 10:55:06 +0800

    Solve the issue that bootclone can't be executed when building image.

--- rules/project/bootclone.mk
+++ rules/project/bootclone.mk
@@ -4,7 +4,7 @@ bootclone: $(builddir)/.bootclone
 
 define bootclonetemplate
 $(builddir)/.bootclone_$(1): $(bootfs)
-	@( tar -C $(currdir)/srcs/boot/$(1) --numeric-owner -zcpvf - . | tar -C $(bootfs) -zxpvf - ) > /dev/null 2>&1
+	@( tar -C $(currdir)/srcs/boot/$(1) --numeric-owner --no-same-owner -zcpvf - . | tar -C $(bootfs) --no-same-owner -zxpvf - ) > /dev/null 2>&1
 endef
 
 $(foreach r,$(bootclone),$(eval $(call bootclonetemplate,$(r))))
